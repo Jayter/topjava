d6d58945d8f5281360bf1f9bd51f5c0f5065c58c
Jayton <kompovich@gmail.com>
2016-09-26 14:16:06 +0300

    Added srcipt for creating and populating table with meals.

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
+DROP SEQUENCE IF EXISTS global_meal_seq;
 
 CREATE SEQUENCE global_seq START 100000;
 
@@ -22,4 +24,16 @@ CREATE TABLE user_roles
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE SEQUENCE global_meal_seq START 100;
+
+CREATE TABLE meals
+(
+  id              INTEGER PRIMARY KEY DEFAULT nextval('global_meal_seq'),
+  user_id         INTEGER NOT NULL,
+  localDateTime   TIMESTAMP DEFAULT now(),
+  description     VARCHAR NOT NULL,
+  calories        VARCHAR NOT NULL DEFAULT 500,
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
