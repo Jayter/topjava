662e913156a347a5ff9dfc41f623f8f1dcc67b1b
Jayton <kompovich@gmail.com>
2016-10-28 13:29:49 +0300

    Created test for CSS.

--- src/test/java/ru/javawebinar/topjava/web/resources/ResourceControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/resources/ResourceControllerTest.java
@@ -1,10 +1,13 @@
 package ru.javawebinar.topjava.web.resources;
 
 import org.junit.Test;
+import org.springframework.http.MediaType;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by Jayton on 26.10.2016.
@@ -12,7 +15,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 public class ResourceControllerTest extends AbstractControllerTest {
     @Test
     public void testStyle() throws Exception {
-        mockMvc.perform(get("style.css"))
-                        .andDo(print());
+        mockMvc.perform(get("/resources/css/style.css"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("text/css")));
     }
 }
